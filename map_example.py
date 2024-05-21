491df3b4b4c4b31561f0880004d3c82c099a93a5
Aashish-993 <aashishshrestha993.com>
2024-05-21 17:22:36 +0545

    file updated

---
+++ map_example.py
@@ -0,0 +1,26 @@
+def capiyalize_and_ascii_sum(word: str):
+    return sum(ord(x) for x in word.capitalize())
+
+animals =['dog', 'cat' ,'cow']
+
+### normal way
+#animal_output = []
+#
+#for animal in animals:
+#    asci_value = capiyalize_and_ascii_sum(animal)
+#    animal_output.append(asci_value)        
+#
+#print(animal_output)
+
+
+## using map
+animal_output = list(map(capiyalize_and_ascii_sum, animals))
+print(animal_output)
+
+
+###  square 
+
+numbers = [1, 2, 3, 4, 5]
+
+square = map(lambda x: x ** 2, numbers)
+print(list(square))
