1931f59dcbdbdc679f135f9a3b6449757b9534ad
Aashish-993 <aashishshrestha993.com>
2024-05-22 17:32:13 +0545

    file updated

--- sqlite_example.py
+++ sqlite_example.py
@@ -1,4 +1,34 @@
 import sqlite3
 
-sqliteConnection = sqlite3.connect('first_eaxample.db')
+sqliteConnection = sqlite3.connect('first_example.db')
 print("Database connected")
+
+cursor = sqliteConnection.cursor()
+print("Database initialized")
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS emp (id integer primary key AUTOINCREMENT, name text, address text, age int):
+"""
+cursor.execute(create_table_query)
+
+insert_table_query = """
+INSERT INTO emp(name, address, age) VALUES("Aashsih", "lalitpur" , 200) 
+"""
+
+insert_table_query = """
+INSERT INTO emp(name, address, age) VALUES("Aashsih", "lalitpur" , 200) 
+"""
+
+insert_table_query = """
+INSERT INTO emp(name, address, age) VALUES("Aashsih", "lalitpur" , 200) 
+"""
+
+insert_table_query = """
+INSERT INTO emp(name, address, age) VALUES("Aashsih", "lalitpur" , 200) 
+"""
+
+cursor.execute(insert_table_query)
+sqliteConnection.commit()
+
+
+sqliteConnection.close()
